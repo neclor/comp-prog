6adadf5d474b6dccac6b5e3ad43b27c965586212
neclor <neclordev@gmail.com>
2025-04-06 14:58:44 +0200

    abcd

--- project_1/code/main-prefixe_suffixe.c
+++ project_1/code/main-prefixe_suffixe.c
@@ -7,6 +7,9 @@
 #define N2 10
 #define N3 9
 
+#define N4 17
+#define N5 5
+
 
 int main(){
    // const unsigned int N1 = 9;
@@ -17,14 +20,13 @@ int main(){
    int T2[N2] = {1,2,3,2,1,1,2,3,2,1};
    int T3[N3] = {3,2,3,2,1,2,3,2,1};
 
+   int T4[N4] = {1,2,3,4,1,2,3,1,2,3,4,1,2,3,4,1,2};
+   int T5[N5] = {0,0,0,0,0};
+
    printf("Length longest prefix/suffix of T1: %u\n", prefixe_suffixe(T1, N1)); // 4
    printf("Length longest prefix/suffix of T2: %u\n", prefixe_suffixe(T2, N2)); // 5
    printf("Length longest prefix/suffix of T3: %u\n", prefixe_suffixe(T3, N3)); // 0
-
-
-   int my_test_1[17] = {1,2,3,4,1,2,3,1,2,3,4,1,2,3,4,1,2};
-   printf("My test 1: %u\n", prefixe_suffixe(my_test_1, 17)); // 6
-
-   int my_test_2[5] = {0,0,0,0,0};
-   printf("My test 2: %u\n", prefixe_suffixe(my_test_2, 5)); // 4
+   printf("\n");
+   printf("Length longest prefix/suffix of T4: %u\n", prefixe_suffixe(T4, N4)); // 6
+   printf("Length longest prefix/suffix of T5: %u\n", prefixe_suffixe(T5, N5)); // 4
 }
